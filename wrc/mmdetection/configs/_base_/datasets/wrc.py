d3d8d2bca3329d6288d7ffa3a3e9ba7ae8719f7f
gyro永不抽风 <1247006353@qq.com>
2022-07-17 20:59:13 +0800

    fix: update path

--- wrc/mmdetection/configs/_base_/datasets/wrc.py
+++ wrc/mmdetection/configs/_base_/datasets/wrc.py
@@ -1,6 +1,6 @@
 # dataset settings
 dataset_type = 'wrc'
-data_root = '/home/ma-user/work/wrc/data/labeled/'
+data_root = '../data/labeled/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
--- wrc/mmdetection/configs/_base_/default_runtime.py
+++ wrc/mmdetection/configs/_base_/default_runtime.py
@@ -11,6 +11,6 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = "/home/ma-user/work/wrc/data/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth"  
+load_from = "../data/mask_rcnn_r50_fpn_1x_coco_20200205-d4b0c5d6.pth"  
 resume_from = None
 workflow = [('train', 1)]
