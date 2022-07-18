f081e861eb147f5a8c51803030d5da7187083f1e
gyro永不抽风 <1247006353@qq.com>
2022-07-18 13:01:05 +0800

    feat: add albumentations dependency

--- install.ps1
+++ install.ps1
@@ -4,4 +4,5 @@ pip install imgviz labelme
 pip install torch==1.8.2 torchvision==0.9.2 torchaudio===0.8.2 --extra-index-url https://download.pytorch.org/whl/lts/1.8/cu111
 pip install -U openmim
 mim install mmcv-full==1.4.0
+pip install -U albumentations
 cd .\wrc\mmdetection ; python setup.py install
