94a5296a969f90ee4938d553b839baab90bf4b27
gyro永不抽风 <1247006353@qq.com>
2022-07-19 21:06:37 +0800

    feat: add more training policies

--- wrc/mmdetection/configs/mask_rcnn/wrc_resnet101_50e.py
+++ wrc/mmdetection/configs/mask_rcnn/wrc_resnet101_50e.py
@@ -1,5 +1,5 @@
 _base_ = [
     '../_base_/models/mask_rcnn_x101_32x8d_fpn_mstrain-poly_3x_coco.py',
     '../_base_/datasets/wrc.py',
-    '../_base_/schedules/schedule_resnet101_20e.py', '../_base_/runtime_resnet101.py'
+    '../_base_/schedules/schedule_resnet101_50e.py', '../_base_/runtime_resnet101.py'
 ]
--- wrc/mmdetection/configs/mask_rcnn/wrc_resnet101_cosine_75e.py
+++ wrc/mmdetection/configs/mask_rcnn/wrc_resnet101_cosine_75e.py
@@ -1,5 +1,5 @@
 _base_ = [
-    '../_base_/models/mask_rcnn_x101_32x8d_fpn_mstrain-poly_1x_coco.py',
+    '../_base_/models/mask_rcnn_x101_32x8d_fpn_mstrain-poly_3x_coco.py',
     '../_base_/datasets/wrc.py',
     '../_base_/schedules/schedule_resnet101_cosine_75e.py', '../_base_/runtime_resnet101.py'
 ]
